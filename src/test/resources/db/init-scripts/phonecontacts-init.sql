a283b0b9d8d231f26120241c6075648aef837720
Oleksii Synelnyk <osynelnyk@griddynamics.com>
2023-08-02 17:07:59 +0300

    delete unnecessary init.sql for tests

--- src/test/resources/db/init-scripts/phonecontacts-init.sql
+++
@@ -1,6 +0,0 @@
-
--- INSERT INTO postgres.contacts_db.users (login, password, role)
--- VALUES ('Oleksii', 'pass123', 'USER');
---
--- INSERT INTO postgres.contacts_db.contacts (id, name, user_id)
--- VALUES (1, 'Petya', 1);
